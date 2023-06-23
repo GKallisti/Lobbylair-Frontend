426de428d86110eb6da0b963cb1a6e91e6fff7be
GgKallisti <riptheraven@gmail.com>
2023-06-22 23:58:37 -0300

    Update ProctectedRoutes.jsx

--- src/router/ProctectedRoutes.jsx
+++ src/router/ProctectedRoutes.jsx
@@ -2,12 +2,12 @@ import { Navigate, Outlet } from "react-router-dom";
 
 const ProtectedRoutes = () => {
   const isAuthenticated = localStorage.getItem("isAuthenticated");
-  return isAuthenticated === 'true' ? <Outlet /> : <Navigate to="/" />;
+  return isAuthenticated === 'true' ? <Outlet /> : <Navigate to="/home"  />;
 };
 
 const ProtectedRoutes2 = () => {
   const isAuthenticated = localStorage.getItem("isAuthenticated");
-  return isAuthenticated === 'false' ? <Outlet /> : <Navigate to="/home" />;
+  return isAuthenticated === 'false' ? <Outlet /> : <Navigate to="/"  />;
 };
 
 export { ProtectedRoutes, ProtectedRoutes2 };
